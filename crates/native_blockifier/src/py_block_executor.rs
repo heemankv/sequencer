ab0df32c5cb048646143f42f432b43f1ace149da
einat-starkware <einat@starkware.co>
2025-07-09 09:53:58 +0300

    blockifier: add l3 field to ChainInfo (#7729)

--- crates/native_blockifier/src/py_block_executor.rs
+++ crates/native_blockifier/src/py_block_executor.rs
@@ -460,6 +460,7 @@ impl TryFrom<PyOsConfig> for ChainInfo {
                     py_os_config.fee_token_address.0,
                 )?,
             },
+            is_l3: false,
         })
     }
 }
